1bb50aae187c6901b8348049c7319e9ea43e2a1e
ghaff <ghaff@LAPTOP-OUUVA253>
2019-04-12 14:02:25 +0100

    Installed HTTP Libarry Axios, Fetched some data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/test'
+import PopcornAPI from './service/PopcornAPI'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Test/>
+        <PopcornAPI/>
       </div>
     );
   }
