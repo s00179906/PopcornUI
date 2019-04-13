967760e9cb8b50105f5a29a04838a5dea81c1c89
ghaff <ghaff@LAPTOP-OUUVA253>
2019-04-13 17:02:16 +0100

    changes

--- src/service/PopcornAPI.jsx
+++ src/service/PopcornAPI.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Iframe from "react-iframe";
 import "../styles/PopcornAPI.css";
+import { Link } from "react-router-dom";
 
 class PopcornAPI extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      movieID: ""
     };
   }
 
   componentDidMount() {
-    // axios.get('https://tv-v2.api-fetch.website/movies/')
     axios
       .get("https://tv-v2.api-fetch.website/movies/1")
       .then(res => {
@@ -25,6 +25,11 @@ class PopcornAPI extends Component {
       });
   }
 
+  handleClick = (e, data) => {
+    const movieImdbID = data.imdb_id;
+    localStorage.setItem('movieImdbID', movieImdbID)
+  };
+
   render() {
     const { data } = this.state;
     return (
@@ -33,14 +38,23 @@ class PopcornAPI extends Component {
           <div className="row">
             {data.length
               ? data.map(data => (
-                  <div className="col-sm" key={data._id}>
-                    <div className="movie-wrap">
-                      <img
-                        className="movie-images"
-                        src={data.images.banner}
-                      />
-                      <h6 className="movie-title text-center text-uppercase">{data.title}</h6>
-                    </div>
+                  <div
+                    className="col-sm p-0 my-2"
+                    key={data._id}
+                    onClick={e => this.handleClick(e, data)}
+                  >
+                    <Link to="/movie">
+                      <div className="movie-wrap my-2">
+                        <img
+                          className="movie-images"
+                          src={data.images.banner}
+                          alt="movie"
+                        />
+                        <h6 className="movie-title text-center text-uppercase">
+                          {data.title}
+                        </h6>
+                      </div>
+                    </Link>
                   </div>
                 ))
               : null}
