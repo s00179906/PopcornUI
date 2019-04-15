863e7b0cb535aaaf3ffe7673109ab26e1932b27e
ghaff <ghaff@LAPTOP-OUUVA253>
2019-04-15 18:12:05 +0100

    c

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import PopcornAPI from "./PopcornAPI";
-import Movie from "./Movie";
-import Search from "./Search";
-import Navbar from "./Navbar";
-import GetTVShows from "./GetTVShows";
-import TVShowInfo from "./TVShowInfo";
+import PopcornAPI from './PopcornAPI';
+import Movie from './Movie';
+import Search from './Search';
+import Navbar from './Navbar';
+import GetTVShows from './GetTVShows';
+import TVShowInfo from './TVShowInfo';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className='App'>
           <Navbar />
-          <Route path="/" exact render={() => <PopcornAPI />} />
-          <Route path="/tvshows" render={() => <GetTVShows />} />
-          <Route path="/tvshowsinfo" render={() => <TVShowInfo />} />
-          <Route path="/movie" render={() => <Movie />} />
-          <Route path="/search" render={() => <Search />} />
+          {/* <Route render={() => <Navbar />} /> */}
+          <Route path='/' exact render={() => <PopcornAPI />} />
+          <Route path='/tvshows' render={() => <GetTVShows />} />
+          <Route path='/tvshowsinfo' render={() => <TVShowInfo />} />
+          <Route path='/movie' render={() => <Movie />} />
+          <Route path='/search' render={() => <Search />} />
         </div>
       </BrowserRouter>
     );
