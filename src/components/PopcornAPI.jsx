ce124a26cbdfc187b7b755541b736e8063883cd3
ghaff <ghaff@LAPTOP-OUUVA253>
2019-04-15 19:12:16 +0100

    zoom effect for images

--- src/components/PopcornAPI.jsx
+++ src/components/PopcornAPI.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import "../styles/PopcornAPI.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../styles/PopcornAPI.css';
 
 class PopcornAPI extends Component {
   constructor(props) {
@@ -9,34 +9,34 @@ class PopcornAPI extends Component {
 
     this.state = {
       data: [],
-      movieID: ""
+      movieID: ''
     };
   }
 
   componentDidMount() {
     let urlArray = [
-      "https://tv-v2.api-fetch.website/movies/1",
-      "https://tv-v2.api-fetch.website/movies/2",
-      "https://tv-v2.api-fetch.website/movies/3",
-      "https://tv-v2.api-fetch.website/movies/4",
-      "https://tv-v2.api-fetch.website/movies/5",
-      "https://tv-v2.api-fetch.website/movies/6",
-      "https://tv-v2.api-fetch.website/movies/7",
-      "https://tv-v2.api-fetch.website/movies/8",
-      "https://tv-v2.api-fetch.website/movies/9",
-      "https://tv-v2.api-fetch.website/movies/10",
-      "https://tv-v2.api-fetch.website/movies/11",
-      "https://tv-v2.api-fetch.website/movies/12",
-      "https://tv-v2.api-fetch.website/movies/13",
-      "https://tv-v2.api-fetch.website/movies/14",
-      "https://tv-v2.api-fetch.website/movies/15",
-      "https://tv-v2.api-fetch.website/movies/16",
-      "https://tv-v2.api-fetch.website/movies/17",
-      "https://tv-v2.api-fetch.website/movies/18",
-      "https://tv-v2.api-fetch.website/movies/19",
-      "https://tv-v2.api-fetch.website/movies/20",
-      "https://tv-v2.api-fetch.website/movies/21",
-      "https://tv-v2.api-fetch.website/movies/22"
+      'https://tv-v2.api-fetch.website/movies/1',
+      'https://tv-v2.api-fetch.website/movies/2',
+      'https://tv-v2.api-fetch.website/movies/3',
+      'https://tv-v2.api-fetch.website/movies/4',
+      'https://tv-v2.api-fetch.website/movies/5',
+      'https://tv-v2.api-fetch.website/movies/6',
+      'https://tv-v2.api-fetch.website/movies/7',
+      'https://tv-v2.api-fetch.website/movies/8',
+      'https://tv-v2.api-fetch.website/movies/9',
+      'https://tv-v2.api-fetch.website/movies/10',
+      'https://tv-v2.api-fetch.website/movies/11',
+      'https://tv-v2.api-fetch.website/movies/12',
+      'https://tv-v2.api-fetch.website/movies/13',
+      'https://tv-v2.api-fetch.website/movies/14',
+      'https://tv-v2.api-fetch.website/movies/15',
+      'https://tv-v2.api-fetch.website/movies/16',
+      'https://tv-v2.api-fetch.website/movies/17',
+      'https://tv-v2.api-fetch.website/movies/18',
+      'https://tv-v2.api-fetch.website/movies/19',
+      'https://tv-v2.api-fetch.website/movies/20',
+      'https://tv-v2.api-fetch.website/movies/21',
+      'https://tv-v2.api-fetch.website/movies/22'
     ];
 
     let promiseArray = urlArray.map(url => axios.get(url));
@@ -56,7 +56,7 @@ class PopcornAPI extends Component {
 
   handleClick = (e, data) => {
     const movieImdbID = data.imdb_id;
-    localStorage.setItem("movieImdbID", movieImdbID);
+    localStorage.setItem('movieImdbID', movieImdbID);
   };
 
   render() {
@@ -64,25 +64,26 @@ class PopcornAPI extends Component {
 
     return (
       <React.Fragment>
-        <div className="container-fluid ml-2">
-          <div className="row">
+        <div className='container-fluid ml-2'>
+          <div className='row'>
             {data.length
               ? data.map(data => (
                   <div
-                    className="col-md col-sm-6 col-xs-6 p-0 my-2"
+                    className='col-md col-sm-6 col-xs-6 p-0 my-2'
                     key={data._id}
-                    onClick={e => this.handleClick(e, data)}
-                  >
-                    <Link to="/movie">
-                      <div className="movie-wrap my-2">
+                    onClick={e => this.handleClick(e, data)}>
+                    <Link to='/movie'>
+                      <div className='movie-wrap my-2 item'>
                         <img
-                          className="movie-images"
+                          className='movie-images'
                           src={data.images.banner}
-                          alt="movie"
+                          alt='movie'
                         />
-                        <h6 className="movie-title text-center text-uppercase">
+                        <div className='text-bg' />
+                        <h6 className='movie-title text-center text-uppercase'>
                           {data.title}
                         </h6>
+                        <div class='item-overlay top' />
                       </div>
                     </Link>
                   </div>
